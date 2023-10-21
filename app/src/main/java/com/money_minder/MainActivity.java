9a2af04d2086b1a7bf0b4e529a819d275f2f8b10
Jayasekaragit <97446071+Jayasekaragit@users.noreply.github.com>
2023-10-21 18:23:33 +0530

    Add files via upload

---
+++ app/src/main/java/com/money_minder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.money_minder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
